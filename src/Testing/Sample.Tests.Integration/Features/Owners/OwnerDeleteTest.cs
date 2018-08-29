01e6e6aa1f58af1009bcdd1530aa8cb6a692409d
Trevor Chunestudy <trevorc@mazamamedia.com>
2018-08-29 15:52:00 -0700

    Owner edit & delete integration tests

---
+++ src/Testing/Sample.Tests.Integration/Features/Owners/OwnerDeleteTest.cs
@@ -0,0 +1,28 @@
+﻿namespace Sample.Tests.Integration.Features.Owners
+{
+    using Sample.Core.Domain;
+    using Sample.Data.Extensions;
+    using Sample.Web.Features.Owners;
+    using Shouldly;
+    using System.Threading.Tasks;
+    using Xunit;
+    using static SliceFixture;
+
+    public class OwnerDeleteTest : IntegrationTestBase
+    {
+        [Fact]
+        public async Task Should_soft_delete_owner()
+        {
+            //Arrange
+            var createdId = await EntitySetupHelper.CreateOwner().ConfigureAwait(false);
+            var command = new Delete.Command { Id = createdId };
+
+            //Act
+            await SendAsync(command).ConfigureAwait(false);
+            var deleted = await ExecuteDbContextAsync<Owner>(x => x.Owners.FirstActiveAsync(y => y.Id == createdId)).ConfigureAwait(false);
+
+            //Assert
+            deleted.ShouldBeNull();
+        }
+    }
+}
---
+++ src/Testing/Sample.Tests.Integration/Features/Owners/OwnerEditTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+
+namespace Sample.Tests.Integration.Features.Admin.Owners
+{
+    using Sample.Core.Domain;
+    using Sample.Web.Features.Owners;
+    using Shouldly;
+    using Xunit;
+    using static SliceFixture;
+
+    public class OwnerEditTest : IntegrationTestBase
+    {
+        [Fact]
+        public async Task Should_edit_owner()
+        {
+            //Arrange
+            var createdId = await EntitySetupHelper.CreateOwner().ConfigureAwait(false);
+            var command = new Edit.Command
+            {
+                Id = createdId,
+                Name = "Foo"
+            };
+
+            //Act
+            await SendAsync(command).ConfigureAwait(false);
+            var edited = await FindAsync<Owner>(createdId);
+
+            //Assert
+            edited.ShouldNotBeNull();
+            edited.Name.ShouldBe("Foo");
+        }
+    }
+}
